07dfdde1a29ad2806f9aa022ee05a8e6ee049af5
VIGNESHRK <93041448+VIGNESH11K@users.noreply.github.com>
2023-12-29 13:09:23 +0530

    layout.tsx update

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className='4xl'>Evently</h1>
+      <Button variant={'destructive'} className='px10'>hello</Button>
+    </main>
+  )
+}
