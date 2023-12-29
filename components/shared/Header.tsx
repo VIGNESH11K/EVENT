336c8deca6fc2a4da67a55622c060819f0db85bd
VIGNESHRK <93041448+VIGNESH11K@users.noreply.github.com>
2023-12-29 20:29:21 +0530

    Auth on local host

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from "next/image"
+import { SignedOut } from '@clerk/nextjs'
+import { Button } from '../ui/button'
 
 const Header = () => {
   return (
     <header className="w-full border-b">
-         <div className="wrapper flex items-center justify-between">
+      <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
         <Image 
             src="public/assets/images/logo.svg" width={128} height={38}
@@ -14,9 +16,16 @@ const Header = () => {
         </Link>
 
         <div className="flex w-32 justify-end gap-3">
+        <SignedOut>
+          <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+           </Button>
+         </SignedOut>
 
         </div>
-        </div>
+      </div>
     </header>
   )
 }
